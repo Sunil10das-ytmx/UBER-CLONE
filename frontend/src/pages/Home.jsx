3f1607dc23b0ced833f29351dded36cf5b0469c2
Sunil Das <rajdas958600@gmail.com>
2026-07-13 00:29:36 +0530

    feat: update Home page layout with logo and map; add input fields for trip location

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import UberLogo from '../assets/Uber-logo.png'
+import ubermap from '../assets/uber-map.gif'
 
 const Home = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <>
+      <div className='h-screen relative'>
+        <img
+        className='w-20 absolute left-5 top-5 text-xl'
+        src={UberLogo}
+        alt='uber logo'
+        />
+
+        <div className='h-screen w-screen'>
+          <img 
+          className='h-full w-full object-cover'
+          src={ubermap} 
+          alt="ubermap" 
+          />
+        </div>
+
+        <div className='bg-white absolute bottom-0 w-full p-5'>
+          <h4 className='text-3xl font-semibold'>Find a trip</h4>
+          <form>
+            <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-2 mb-3' type='text' placeholder='Add a pick up location'/>
+            <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mb-3' type='text' placeholder='Enetr your destination'/>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/UserprotectWrapper.jsx
+++ frontend/src/pages/UserprotectWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import {UserDataContext} from "../context/UserContext";
 import { useContext } from "react";
 import {useNavigate } from "react-router-dom";
