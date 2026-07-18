250a90e26d7705d5aa963b049773f4ca673ef83f
Pratap Das <dask64570@gmail.com>
2026-07-18 23:47:13 +0530

    modifiy WaitingforDriver page

--- frontend/src/Component/WaitingforDriver.jsx
+++ frontend/src/Component/WaitingforDriver.jsx
@@ -2,9 +2,79 @@ import React from 'react'
 
 const WaitingforDriver = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="h-[477.56px]">
+        <h3
+          onClick={() => {
+            props.setvehicleFound(false);
+          }}
+          className="absolute top-4 right-2 cursor-pointer text-2xl"
+        >
+          <i className="ri-close-fill"></i>
+        </h3>
+
+        {/* Vehicle Image */}
+        <div className="flex flex-col gap-4 justify-between items-center mt-8">
+          <motion.img
+            className={`${props.selectedVehicle?.imageClass} h-50 `}
+            src={props.selectedVehicle?.image}
+            alt={props.selectedVehicle?.name}
+          />
+          <div className="flex items-baseline gap-2">
+            <h3 className="text-xl p-2 font-bold leading-none">
+              Searching for nearby drivers
+            </h3>
+
+            <div className="flex items-center gap-1 mt-0.5">
+              <motion.div
+                className="w-2.5 h-2.5 rounded-full bg-blue-500"
+                animate={{ opacity: [0.3, 1, 0.3] }}
+                transition={{ duration: 1, repeat: Infinity }}
+              />
+
+              <motion.div
+                className="w-2.5 h-2.5 rounded-full bg-blue-500"
+                animate={{ opacity: [0.3, 1, 0.3] }}
+                transition={{ duration: 1, repeat: Infinity, delay: 0.2 }}
+              />
+
+              <motion.div
+                className="w-2.5 h-2.5 rounded-full bg-blue-500"
+                animate={{ opacity: [0.3, 1, 0.3] }}
+                transition={{ duration: 1, repeat: Infinity, delay: 0.4 }}
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col mt-3 gap-4">
+            <div className="flex items-center p-2 gap-3">
+              <h3 className="text-2xl">
+                <i className="ri-map-pin-2-fill"></i>
+              </h3>
+              <div>
+                <h3 className="text-2xl font-medium">
+                  {props.address?.place ?? "Unknown place"}
+                </h3>
+                <p className="text-lg text-gray-500">
+                  {props.address?.city ?? "Unknown city"},
+                  {props.address?.state ?? "Unknown state"},
+                  {props.address?.pincode ?? ""}
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-5 p-2">
+              <h3 className="text-2xl">
+                <i className="ri-wallet-2-fill"></i>
+              </h3>
+              <div className="font-bold text-xl">
+                {props.selectedVehicle?.price}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
